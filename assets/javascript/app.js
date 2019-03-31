b83eb634fdbf6f727a6a5a22cdb4491c126abb38
Steve Travis <stevetravis11@gmail.com>
2019-03-30 21:23:12 -0400

    wip: Click function on hero buttons is now working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // Initial array of super heroes
     var superHeroes = ["Avengers", "X-Men", "Justice League", "Iron Man", "Spiderman", "Superman", "Batman", "Incredible Hulk", "Aquaman"];
     // Calling the renderButtons function at least once to display the initial list of movies
-renderButtons();
+    renderButtons();
 
     // Function for displaying buttons
     function renderButtons() {
@@ -12,12 +12,14 @@ renderButtons();
         for (var i = 0; i < superHeroes.length; i++) {
             var hero = $("<button>");
             hero.addClass("hero");
+            $(hero).attr('id', 'hero-button');
+            $(hero).attr('type', 'submit');
             hero.attr("data-name", superHeroes[i]);
             hero.text(superHeroes[i]);
             $("#buttons-view").append(hero);
         }
     }
-    // This function handles events where one button is clicked
+    // Function to add hero buttons
     $("#add-superhero").on("click", function (event) {
         event.preventDefault();
         var hero = $("#button-input").val().trim();
@@ -27,4 +29,25 @@ renderButtons();
         $("form").trigger("reset");
     })
 
+    //This function queries GIPHY for super hero button selected
+    $(".hero").on("click", function (event) {
+        console.log('CLICK');
+        event.preventDefault();
+        // Storing our giphy API URL
+        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=xpKl0hsLaTcPGpvdzolHfqbGD2PgBpqW&tag=hero";
+
+        // Perfoming an AJAX GET request to our queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(response);
+                var imageUrl = response.data.image_original_url;
+                var heroImage = $("<img>");
+                heroImage.attr("src", imageUrl);
+                heroImage.attr("alt", "hero image");
+                $("#images").prepend(heroImage);
+            });
+    });
 });
